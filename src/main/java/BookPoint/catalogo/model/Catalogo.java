8ded11865ebc2da302969188a6968d5c9c78c211
Dani <da.picarte@duocuc.cl>
2026-05-21 14:05:35 -0400

    model catalogo y producto ok

--- src/main/java/BookPoint/catalogo/model/Catalogo.java
+++ src/main/java/BookPoint/catalogo/model/Catalogo.java
@@ -1,12 +1,15 @@
 package BookPoint.catalogo.model;
 
 import java.time.LocalDate;
-
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -20,11 +23,15 @@ import lombok.NoArgsConstructor;
 public class Catalogo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long idCatalogo;
 
-    @Column
+    @Column(nullable = false)
     private String nombreCatalogo;
     
-    @Column
+    @Column(nullable = false)
     private LocalDate fechaActualizacion;
+
+    @OneToMany(mappedBy = "catalogo", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
+    private List<Producto> productos;
 }
--- src/main/java/BookPoint/catalogo/model/Producto.java
+++ src/main/java/BookPoint/catalogo/model/Producto.java
@@ -1,5 +1,58 @@
 package BookPoint.catalogo.model;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Productos")
 public class Producto {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idProducto;
+
+    @Column(nullable = false)
+    private Long idInventario;
+
+    @Column(nullable = false)
+    private String titulo;
+
+    @Column(nullable = false)
+    private String autor;
+
+    @Column(nullable = false)
+    private String editorial;
+
+    @Column(nullable = false)
+    private String descripcion;
+
+    @Column(nullable = false)
+    private String isbn;
+
+    @Column(nullable = false)
+    private Integer precioUnitario;
+
+    @Column(nullable = true)
+    private boolean estado;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_catalogo", nullable = false)
+    @JsonManagedReference
+    @ToString.Exclude
+    private Catalogo catalogo;
 }
