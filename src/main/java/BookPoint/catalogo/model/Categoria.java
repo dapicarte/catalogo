38a41aa2a1115c4b44ea0540217ff4c6c91cfdc8
Dani <da.picarte@duocuc.cl>
2026-05-25 18:40:35 -0400

    todo ok

--- src/main/java/BookPoint/catalogo/model/Categoria.java
+++ src/main/java/BookPoint/catalogo/model/Categoria.java
@@ -2,10 +2,7 @@ package BookPoint.catalogo.model;
 
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
